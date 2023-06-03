b31b2283bcb85519ed8be905db287933e8235f2b
Abdul Samad <76769769+PROGRAMMER-ABDUL@users.noreply.github.com>
2023-06-03 13:27:06 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+var list = document.getElementById("list");
+
+function addTodo() {
+    var todo_item = document.getElementById("todo-item");
+
+
+    // create li tag with text node
+    var li = document.createElement('li')
+    var liText = document.createTextNode(todo_item.value)
+    li.appendChild(liText)
+
+    // create delete button
+    var delBtn = document.createElement("button")
+    var delText = document.createTextNode("DELETE")
+    delBtn.setAttribute("class", "btn")
+    delBtn.setAttribute("onclick", "deleteItem(this)")
+    delBtn.appendChild(delText)
+
+    // create edit button
+    var editBtn = document.createElement("button");
+    var editText = document.createTextNode("EDIT")
+    editBtn.appendChild(editText)
+    editBtn.setAttribute("onclick", "editItem(this)")
+
+
+    li.appendChild(delBtn)
+    li.appendChild(editBtn)
+
+    list.appendChild(li)
+
+    todo_item.value = ""
+}
+
+function deleteItem(e) {
+    e.parentNode.remove()
+}
+
+function deleteAll() {
+    list.innerHTML = ""
+}
